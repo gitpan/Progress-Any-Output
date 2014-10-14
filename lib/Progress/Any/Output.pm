1e965a65f9348a77e0d095ad20adcc46bc52e2e7
perlancar <perlancar@gmail.com>
2014-10-14 08:56:19 +0000

    Import of PERLANCAR/Progress-Any-Output-0.04 from CPAN.
    gitpan-cpan-distribution: Progress-Any-Output
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         PERLANCAR/Progress-Any-Output-0.04.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Progress/Any/Output.pm
+++ lib/Progress/Any/Output.pm
@@ -4,7 +4,7 @@ use 5.010001;
 use strict;
 use warnings;
 
-our $VERSION = '0.03'; # VERSION
+our $VERSION = '0.04'; # VERSION
 
 require Progress::Any;
 
@@ -25,7 +25,7 @@ sub _set_or_add {
     my $task = $opts->{task} // "";
 
     my $outputo;
-    {
+    unless (ref $outputo) {
         my $outputpm = $output; $outputpm =~ s!::!/!g; $outputpm .= ".pm";
         require "Progress/Any/Output/$outputpm";
         no strict 'refs';
@@ -38,6 +38,8 @@ sub _set_or_add {
         $Progress::Any::outputs{$task} //= [];
         push @{ $Progress::Any::outputs{$task} }, $outputo;
     }
+
+    $outputo;
 }
 
 sub set {
@@ -53,10 +55,11 @@ sub add {
 1;
 # ABSTRACT: Assign output to progress indicators
 
-
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -64,7 +67,7 @@ Progress::Any::Output - Assign output to progress indicators
 
 =head1 VERSION
 
-version 0.03
+This document describes version 0.04 of Progress::Any::Output (from Perl distribution Progress-Any-Output), released on 2014-10-14.
 
 =head1 SYNOPSIS
 
@@ -89,12 +92,16 @@ See L<Progress::Any> for overview.
 
 =head1 METHODS
 
-=head2 Progress::Any::Output->set([ \%opts ], $output[, @args])
+=head2 Progress::Any::Output->set([ \%opts ], $output[, @args]) => obj
 
 Set (or replace) output. Will load and instantiate
 C<Progress::Any::Output::$output>. To only set output for a certain (sub)task,
 set C<%opts> to C<< { task => $task } >>. C<@args> will be passed to output
 module's constructor.
+
+Return the instantiated object.
+
+If C<$output> is an object (a reference, really), it will be used as-is.
 
 =head2 Progress::Any::Output->add([ \%opts ], $output[, @args])
 
@@ -104,16 +111,31 @@ Like set(), but will add output instead of replace existing one(s).
 
 L<Progress::Any>
 
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Progress-Any-Output>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-Progress-Any-Output>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Progress-Any-Output>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
 =head1 AUTHOR
 
-Steven Haryanto <stevenharyanto@gmail.com>
+perlancar <perlancar@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Steven Haryanto.
+This software is copyright (c) 2014 by perlancar@cpan.org.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
